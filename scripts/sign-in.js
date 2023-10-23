dd6658fda41990f7a839e3322becd67d3dd96449
Zani231089 <231089@virtualwindow.co.za>
2023-10-23 09:24:52 +0200

    Navigation

--- scripts/sign-in.js
+++ scripts/sign-in.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         if (this.checkValidity() === false){
             event.stopPropagation();
         }else {
-            window.location.href = 'index.html';
+            window.location.href = '';
         }
 
         $(this).addClass('was-validated');
